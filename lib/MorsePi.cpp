efa80a5c7296feeb55d6efbbfbe208b222c4c102
Pobelle pierrick <pierrick.pobelle@gmail.com>
2014-03-31 23:43:55 +0200

    bug fixes
    double declaration :s

--- lib/MorsePi.cpp
+++ lib/MorsePi.cpp
@@ -23,6 +23,20 @@
   *
   */
 #include "MorsePi.h"
+
+int _court = 50; //durée de base cf: http://fr.wikipedia.org/wiki/Morse_(alphabet)
+
+char _lettres[37] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0',' '};
+int _long= 3 * _court;
+int _espaceEntreLettre= 3 * _court;
+int _espaceEntreMot= 7 * _court;
+int _lumieres[37][6] = {{_court,_long},{_long,_court,_court,_court},{_long,_court,_long,_court},{_long,_court,_court},{_court},{_court,_court,_long,_court},{_long,_long,_court},{_court,_court,_court,_court},{_court,_court},
+  {_court,_long,_long,_long},{_long,_court,_long},{_court,_long,_court,_court},{_long,_long},{_long,_court},{_long,_long,_long},{_court,_long,_long,_court},{_long,_long,_court,_long},{_court,_long,_court},
+  {_court,_court,_court},{_long},{_court,_court,_long},{_court,_court,_court,_long},{_court,_long,_long},{_long,_court,_court,_long},{_long,_court,_long,_long},{_long,_long,_court,_court},{_court,_long,_long,_long,_long},
+  {_court,_court,_long,_long,_long},{_court,_court,_court,_long,_long},{_court,_court,_court,_court,_long},{_court,_court,_court,_court,_court},{_long,_court,_court,_court,_court},{_long,_long,_court,_court,_court},{_long,_long,_long,_court,_court},{_long,_long,_long,_court},
+  {_long,_long,_long,_long,_long},{0}};
+
+
 /**
  * MorseLedPi constructor
  * @param char * str a char* to transform in morseCode
--- lib/MorsePi.h
+++ lib/MorsePi.h
@@ -4,19 +4,6 @@
 #include <string.h>
 using namespace std;
 
-int _court = 50; //durée de base cf: http://fr.wikipedia.org/wiki/Morse_(alphabet)
-
-char _lettres[37] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0',' '};
-int _long= 3 * _court;
-int _espaceEntreLettre= 3 * _court;
-int _espaceEntreMot= 7 * _court;
-int _lumieres[37][6] = {{_court,_long},{_long,_court,_court,_court},{_long,_court,_long,_court},{_long,_court,_court},{_court},{_court,_court,_long,_court},{_long,_long,_court},{_court,_court,_court,_court},{_court,_court},
-  {_court,_long,_long,_long},{_long,_court,_long},{_court,_long,_court,_court},{_long,_long},{_long,_court},{_long,_long,_long},{_court,_long,_long,_court},{_long,_long,_court,_long},{_court,_long,_court},
-  {_court,_court,_court},{_long},{_court,_court,_long},{_court,_court,_court,_long},{_court,_long,_long},{_long,_court,_court,_long},{_long,_court,_long,_long},{_long,_long,_court,_court},{_court,_long,_long,_long,_long},
-  {_court,_court,_long,_long,_long},{_court,_court,_court,_long,_long},{_court,_court,_court,_court,_long},{_court,_court,_court,_court,_court},{_long,_court,_court,_court,_court},{_long,_long,_court,_court,_court},{_long,_long,_long,_court,_court},{_long,_long,_long,_court},
-  {_long,_long,_long,_long,_long},{0}};
-
-
 class MorseLedPi{
   public:
     MorseLedPi(char* str, int pin);
@@ -26,7 +13,5 @@ class MorseLedPi{
   protected:
     int pin_;
     char *mot_;
-
-
 
 };
